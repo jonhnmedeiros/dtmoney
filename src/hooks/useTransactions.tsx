e511145b3b6efe587de6ee69234f5c53a87f3e1c
Jonathan Medeiros <jonathan.info@gmail.com>
2021-05-24 09:50:02 -0300

    Add hooks, app finished

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useEffect, useState } from 'react';
-import {api} from './services/api';
+import { createContext, ReactNode, useEffect, useState, useContext } from 'react';
+import {api} from '../services/api';
 
 interface Transaction {
     id: number;
@@ -30,7 +30,7 @@ interface TransactionsContextData {
     createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
     {} as TransactionsContextData
 );
 
@@ -63,5 +63,10 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 
 }
 
+export function useTransactions() {
+    const context = useContext(TransactionsContext);
+
+    return context;
+}
 
 
